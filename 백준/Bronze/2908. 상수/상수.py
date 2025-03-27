bdbeb1c93bd44024887da291f09076c686c39dcc
zhwan2845 <johaeun0802@naver.com>
2025-03-27 15:44:12 +0900

    [Bronze II] Title: 상수, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+new_a = ""
+new_b = ""
+
+for  i in range(2, -1, -1):
+    new_a += a[i]
+    new_b += b[i]
+if int(new_a) > int(new_b):
+    print(int(new_a))
+else:
+    print(int(new_b))
