46fa97df8458f6731f35217dc826870fcf98b0f4
zhwan2845 <johaeun0802@naver.com>
2026-01-17 00:11:16 +0900

    [Silver II] Title: 로또, Time: 276 ms, Memory: 16960 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int k;
     static int[] s;
     static int[] lotto;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
-            k = sc.nextInt();
+            String line = br.readLine();
+            if (line == null) break;
+
+            StringTokenizer st = new StringTokenizer(line);
+            k = Integer.parseInt(st.nextToken());
             if (k == 0) break;
 
             s = new int[k];
             lotto = new int[6];
 
             for (int i = 0; i < k; i++) {
-                s[i] = sc.nextInt();
+                s[i] = Integer.parseInt(st.nextToken());
             }
 
             backtracking(0, 0);
